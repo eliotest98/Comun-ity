c78e5187fddb291dc5e7e5aae2d18082c32a43dd
Alessandro Falcone <100565946+AlessandroFalcone01@users.noreply.github.com>
2022-12-30 09:51:42 +0100

    correzione nome classe DbConnection

--- src/main/java/controller/gestioneUtenza/IndexServlet.java
+++ src/main/java/controller/gestioneUtenza/IndexServlet.java
@@ -6,10 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.mongodb.client.MongoDatabase;
-
-import controller.utility.DBConnection;
+import controller.utility.DbConnection;
 
 /**
  * Servlet implementation class IndexServlet
@@ -32,7 +30,7 @@ public class IndexServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("/Comun-ity/index.jsp");
 		
-		MongoDatabase db = DBConnection.connectToDb();
+		MongoDatabase db = DbConnection.connectToDb();
 		
 		System.out.println("Connesso");
 		
