1e2fd2e32f40cbce3e513ad6a5c190c3a97962a4
MicheleRabesco <m.rabesco@studenti.unisa.it>
2023-01-05 12:01:01 +0100

    Setup Test Files per il versioning

---
+++ src/test/java/controller/gestioneAnnunci/PresaInCaricoAnnuncioServletTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package controller.gestioneAnnunci;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author miche
+ *
+ */
+class PresaInCaricoAnnuncioServletTest {
+
+	/**
+	 * Test method for {@link controller.gestioneAnnunci.PresaInCaricoAnnuncioServlet#PresaInCaricoAnnuncioServlet()}.
+	 */
+	@Test
+	void testPresaInCaricoAnnuncioServlet() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.gestioneAnnunci.PresaInCaricoAnnuncioServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)}.
+	 */
+	@Test
+	void testDoGetHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controller.gestioneAnnunci.PresaInCaricoAnnuncioServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)}.
+	 */
+	@Test
+	void testDoPostHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+}
