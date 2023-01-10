565b5c8b75e911c8de70c338a838b8511cb77981
Alessandro Falcone <100565946+AlessandroFalcone01@users.noreply.github.com>
2023-01-10 11:57:53 +0100

    accertamento professionista servlet

--- src/main/java/controller/gestioneAccreditamento/InserimentoCertificazione.java
+++ src/main/java/controller/gestioneAccreditamento/InserimentoCertificazione.java
@@ -22,110 +22,116 @@ import model.Utente;
  */
 @WebServlet("/InserimentoCertificazione")
 public class InserimentoCertificazione extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public InserimentoCertificazione() {
-        super();
-        // TODO Auto-generated constructor stub
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public InserimentoCertificazione() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  GestioneAccreditamentoService serviceA = new GestioneAccreditamentoServiceImpl();
+  GestioneUtenzaService serviceU = new GestioneUtenzaServiceImpl();
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    HttpSession session = request.getSession(true);
+    Utente user = (Utente) session.getAttribute("user");
+
+    if (user == null) {
+      response.sendRedirect("/Comun-ity/guest/login.jsp");
+    } else {
+      response.sendRedirect(" "); //jsp inserimento certificazione
     }
-    
-    GestioneAccreditamentoService serviceA= new GestioneAccreditamentoServiceImpl();
-    GestioneUtenzaService serviceU= new GestioneUtenzaServiceImpl();
+  }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		HttpSession session = request.getSession(true);
-	    Utente user = (Utente) session.getAttribute("user");
-	    
-	    if (user == null) {
-	        response.sendRedirect("/Comun-ity/guest/login.jsp");
-	      } else {
-	        response.sendRedirect(" "); //jsp inserimento certificazione
-	      }
-	}
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
-		HttpSession session = request.getSession(true);
-	    Utente user = (Utente) session.getAttribute("user");
-	    
-		String utente= user.getMail(); //mail dell'utente che sottomette la richiesta
-		String abilitazione= request.getParameter("abilitazione"); //abilitazione che si suppone di avere
-		String allegato= request.getParameter("allegato"); //file della certificazione
-		
-		if(user.getRuolo().equals("cittadino")) {
-			
-			if(abilitazioneOK(abilitazione)) {
-				
-				if(allegatoOK(allegato)) {
-					
-					Accreditamento accreditamento= new Accreditamento(utente, abilitazione, allegato);
-					
-					serviceA.saveAccreditamento(accreditamento);
-					
-					RequestDispatcher requestDispatcher = request.getRequestDispatcher("");	//jsp inserimento certificazione
-				    request.setAttribute("success", "Richiesta sottomessa con successo, verra' controllata il prima possibile");
+    HttpSession session = request.getSession(true);
+    Utente user = (Utente) session.getAttribute("user");
 
-				    requestDispatcher.forward(request, response);
-					
-				}else {
-					
-					RequestDispatcher requestDispatcher = request.getRequestDispatcher("");	//jsp inserimento certificazione
-				    request.setAttribute("error", "Errore nell'inserimento dell'allegato, deve essere un pdf da massimo 50MB");
+    String utente = user.getMail(); //mail dell'utente che sottomette la richiesta
+    String abilitazione = request.getParameter("abilitazione"); //abilitazione che si suppone di avere
+    String allegato = request.getParameter("allegato"); //file della certificazione
 
-				    requestDispatcher.forward(request, response);
-					
-				}
-				
-			}else {
-				
-				RequestDispatcher requestDispatcher = request.getRequestDispatcher("");	//jsp inserimento certificazione
-			    request.setAttribute("error", "Formato abilitazione non corretto");
+    if (user.getRuolo().equals("cittadino")) {
 
-			    requestDispatcher.forward(request, response);
-				
-			}
-			
-		}else {
-			
-			RequestDispatcher requestDispatcher = request.getRequestDispatcher("/Comun-ity/guest/login.jsp");	
-		    request.setAttribute("error", "Solo un cittadino puo' sottomettere una richiesta di accreditamento");
+      if (abilitazioneOK(abilitazione)) {
 
-		    requestDispatcher.forward(request, response);
-			
-		}
-	}
-	
-	public static boolean abilitazioneOK(String abilitazione) {
-		
-		boolean res= true;
-		
-		if(abilitazione.length()<1 || abilitazione.length()>30) {
-			res= false;
-		}
-		
-		return res;
-	}
-	
-	public static boolean allegatoOK(String allegato) {
-		boolean res= true;
-		byte[] data = DatatypeConverter.parseBase64Binary(allegato);
-		
-		if(data.length < 0 || data.length > 52428800) {
-			res= false;
-		}
-		
-		return res;
-	}
+        if (allegatoOK(allegato)) {
+
+          Accreditamento accreditamento = new Accreditamento(utente, abilitazione, allegato);
+
+          serviceA.saveAccreditamento(accreditamento);
+
+          RequestDispatcher requestDispatcher = request.getRequestDispatcher("");	//jsp inserimento certificazione
+          request.setAttribute("success",
+              "Richiesta sottomessa con successo, verra' controllata il prima possibile");
+
+          requestDispatcher.forward(request, response);
+
+        } else {
+
+          RequestDispatcher requestDispatcher = request.getRequestDispatcher("");	//jsp inserimento certificazione
+          request.setAttribute("error",
+              "Errore nell'inserimento dell'allegato, deve essere un pdf da massimo 50MB");
+
+          requestDispatcher.forward(request, response);
+
+        }
+
+      } else {
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("");	//jsp inserimento certificazione
+        request.setAttribute("error", "Formato abilitazione non corretto");
+
+        requestDispatcher.forward(request, response);
+
+      }
+
+    } else {
+
+      RequestDispatcher requestDispatcher = request.getRequestDispatcher(
+          "/Comun-ity/guest/login.jsp");
+      request.setAttribute("error",
+          "Solo un cittadino puo' sottomettere una richiesta di accreditamento");
+
+      requestDispatcher.forward(request, response);
+
+    }
+  }
+
+  public static boolean abilitazioneOK(String abilitazione) {
+
+    boolean res = true;
+
+    if (abilitazione.length() < 1 || abilitazione.length() > 30) {
+      res = false;
+    }
+
+    return res;
+  }
+
+  public static boolean allegatoOK(String allegato) {
+    boolean res = true;
+    byte[] data = DatatypeConverter.parseBase64Binary(allegato);
+
+    if (data.length < 0 || data.length > 52428800) {
+      res = false;
+    }
+
+    return res;
+  }
 
 }
--- src/main/java/controller/gestioneAnnunci/AnnuncioCompletato.java
+++ src/main/java/controller/gestioneAnnunci/AnnuncioCompletato.java
@@ -19,51 +19,53 @@ import model.Utente;
  */
 @WebServlet("/AnnuncioCompletato")
 public class AnnuncioCompletato extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AnnuncioCompletato() {
-        super();
-        // TODO Auto-generated constructor stub
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public AnnuncioCompletato() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  GestioneAnnunciService serviceA = new GestioneAnnunciServiceImpl();
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    HttpSession session = request.getSession(true);
+    Utente utente = (Utente) session.getAttribute("user");
+
+    if (utente == null) {
+      response.sendRedirect("/Comun-ity/guest/login.jsp");
+    } else {
+      response.sendRedirect("HomeServlet");
     }
-    
-    GestioneAnnunciService serviceA = new GestioneAnnunciServiceImpl();
+  }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		HttpSession session = request.getSession(true);
-	    Utente utente = (Utente) session.getAttribute("user");
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    Long id = (Long.parseLong(request.getParameter("annuncio")));
 
-	    if (utente == null) {
-	      response.sendRedirect("/Comun-ity/guest/login.jsp");
-	    } else {
-	      response.sendRedirect("HomeServlet"); 
-	    }
-	}
+    if (serviceA.markAsDone(id)) {
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		Long id = (Long.parseLong(request.getParameter("annuncio")));
-		
-		if(serviceA.markAsDone(id)) {
-			
-			RequestDispatcher requestDispatcher = request.getRequestDispatcher("HomeServlet");
-			request.setAttribute("success", "Annuncio marcato come completato con successo");
-			requestDispatcher.forward(request, response);
-			
-		}else {
-			RequestDispatcher requestDispatcher = request.getRequestDispatcher("HomeServlet");
-		    request.setAttribute("error", "Non è stato possibile marcare l'annuncio come completato");
-		    requestDispatcher.forward(request, response);
-		}
-	}
+      RequestDispatcher requestDispatcher = request.getRequestDispatcher("HomeServlet");
+      request.setAttribute("success", "Annuncio marcato come completato con successo");
+      requestDispatcher.forward(request, response);
+
+    } else {
+      RequestDispatcher requestDispatcher = request.getRequestDispatcher("HomeServlet");
+      request.setAttribute("error", "Non è stato possibile marcare l'annuncio come completato");
+      requestDispatcher.forward(request, response);
+    }
+  }
 
 }
