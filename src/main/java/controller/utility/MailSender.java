ded99b1b2689d0cf1397f43cb818bea2ce2d5ea2
Alessandro Falcone <100565946+AlessandroFalcone01@users.noreply.github.com>
2023-01-11 11:30:05 +0100

    CheckStyle MailSender

--- src/main/java/controller/utility/MailSender.java
+++ src/main/java/controller/utility/MailSender.java
@@ -1,9 +1,9 @@
 package controller.utility;
 
-import java.util.ArrayList;
+import controller.gestioneUtenza.GestioneUtenzaService;
+import controller.gestioneUtenza.GestioneUtenzaServiceImpl;
 import java.util.List;
 import java.util.Properties;
-
 import javax.mail.Address;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -12,12 +12,11 @@ import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-
-import controller.gestioneUtenza.GestioneUtenzaService;
-import controller.gestioneUtenza.GestioneUtenzaServiceImpl;
 import model.Accreditamento;
 import model.Annuncio;
 import model.Utente;
+
+
 
 /**
  * Java class that defines static methods for email notification.
@@ -28,7 +27,7 @@ public class MailSender {
    * Sends a notification email to the given ad author to let him know
    * that the given user has taken on his ad.
    *
-   * @param utente   is the user object to extract user datas from.
+   * @param utente is the user object to extract user datas from.
    * @param annuncio is the ad object to extract ad datas from.
    */
   public static void notifyAdTakeOn(Utente utente, Annuncio annuncio) {
@@ -65,18 +64,17 @@ public class MailSender {
       e.printStackTrace();
     }
   }
-
-
+  
+  
   /**
    * Sends a notification email to admins to let them know
    * there is a new accreditation request.
    *
    * @param accreditamento is the ad object to extract ad datas from.
    */
-  public static void notifyAccreditationReq(Accreditamento accreditamento) throws AddressException,
-      MessagingException {
-    //TODO
-
+  public static void notifyAccreditationReq(Accreditamento accreditamento) 
+      throws AddressException, MessagingException {
+    
     String host = "smtp.gmail.com";
 
     Properties properties = System.getProperties();
@@ -91,13 +89,13 @@ public class MailSender {
             "uslzhowdfvoknjum");
       }
     });
-
+    
     try {
       GestioneUtenzaService service = new GestioneUtenzaServiceImpl();
       List<String> admins = service.getAllAdminsEmails();
-
+    
       String subject = "Nuova richiesta di accreditamento sottomoessa!";
-
+    
       MimeMessage message = new MimeMessage(session);
       for (String e : admins) {
         message.setFrom(new InternetAddress(e));
@@ -107,19 +105,18 @@ public class MailSender {
       message.setSubject(subject);
       message.setText(createBody1(accreditamento));
       Transport.send(message);
-
+    
       for (String e : admins) {
-        System.out.println("Email to: " + e + " SENT.");
-        ;
+        System.out.println("Email to: " + e + " SENT.");;
       }
-
+    
     } catch (Exception e) {
       System.out.println("Error, emails not sent.");
       e.printStackTrace();
     }
-
-
-
+    
+    
+  
   }
 
   /**
@@ -137,8 +134,8 @@ public class MailSender {
         .getNome() + " " + utente.getCognome();
     return text;
   }
-
-
+  
+  
   /**
    * Creates the email body.
    *
@@ -149,11 +146,12 @@ public class MailSender {
   private static String createBody1(Accreditamento accreditamento) {
     String text;
 
-    text =
-        "E' stata sottomessa una nuova richiesta di accreditamento.\n" + "L'utente " + accreditamento
-            .getRichiedente() + ", \n" + " ha sottomoesso una richiesta di accreditamento al fine di essere riconosciuto dal sistema come " + accreditamento
-                .getAbilitazione() + ".";
-
+    text = "E' stata sottomessa una nuova richiesta di accreditamento.\n" 
+        + "L'utente " + accreditamento.getRichiedente() + ", \n" 
+        + " ha sottomoesso una richiesta di" 
+        + "accreditamento al fine di essere riconosciuto dal sistema come " 
+        + accreditamento.getAbilitazione() + ".";
+        
     return text;
   }
 
